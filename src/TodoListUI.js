92dadef6417011984d6c78ddce95aa05a2c470ab
xuey <844534780@qq.com>
2018-12-20 22:36:37 +0800

    将class的UI组件转换为无状态的函数组件

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -4,28 +4,27 @@ import {Button, Input, List} from 'antd';
 
 import 'antd/dist/antd.css';
 
-class TodoListUI extends Component{
-    render(){
-        return (
-            <div className="todolist"  style={{padding: 30}}>
-                <Input
-                    style={{ width: 200, marginRight: 20 }}
-                    placeholder="input search text"            
-                    value={this.props.inputValue}        
-                    onChange={this.props.changeInput}
-                />
-                <Button  style={{marginLeft:30+'px'}}  type="primary"    onClick={this.props.handleBtnClick}>提交</Button>
-                <br/>
-                <br/>
-                <List                
-                    style={{width: 600}}
-                    bordered
-                    dataSource={this.props.list}
-                    renderItem={(item, index) => (<List.Item  onClick={(e)=>this.props.delItem(index) }  >{item}</List.Item>)}
-                />
-            </div>
-        );
-    }
+
+const TodoListUI = (props)=>{
+    return (
+        <div className="todolist"  style={{padding: 30}}>
+            <Input
+                style={{ width: 200, marginRight: 20 }}
+                placeholder="input search text"            
+                value={props.inputValue}        
+                onChange={props.changeInput}
+            />
+            <Button  style={{marginLeft:30+'px'}}  type="primary"    onClick={props.handleBtnClick}>提交</Button>
+            <br/>
+            <br/>
+            <List                
+                style={{width: 600}}
+                bordered
+                dataSource={props.list}
+                renderItem={(item, index) => (<List.Item  onClick={(e)=>props.delItem(index) }  >{item}</List.Item>)}
+            />
+        </div>
+    );
 }
 
 export default TodoListUI;
